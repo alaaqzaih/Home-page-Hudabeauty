925fbc6b5eda7440f44f6620c3f4504e45370f91
alaaqz <alaa.qzaih@gmail.com>
2023-03-09 14:00:39 +0200

    update

--- src/components/productCard/Product.js
+++ src/components/productCard/Product.js
@@ -2,38 +2,32 @@ import React from "react";
 import './productCard.scss'
 export default function ProductCard(props) {
   return (
-    <section>
       <div className="container">
         <a className="product-card" href="">
           <div className="cardImages">
             <img className="product-card__image" src={props.item.productImage} />
             <img className="product-card__image-hover" src={props.item.productImagehover} />
-            <div className="card-badge">
-              <div className="badges-container">
-                <span className="white-tile-badge">
-                  NEW!
-                </span>
-                { props.item.save && ( <span className="pink-title-badge">
-                  Save {props.item.saveValue}
-                </span>)}
+            <div className="badges-container">
+              <div className="white-tile-badge">
+                NEW!
               </div>
-              <a href="" className="quick-view" title='quick-view'>
-                <span>Quick View</span>
-              </a>
+              {props.item.save && (<div className="pink-title-badge">
+                Save {props.item.saveValue}
+              </div>)}
             </div>
+            <a href="" className="quick-view" title='quick-view'>
+              <div>Quick View</div>
+            </a>
           </div>
         </a>
         <div className="title_body">
           <div className="product-title">{props.item.title}</div>
           <div className="product-description">{props.item.description}</div>
-          <div className='add-to-cart-container'>
-            <button className='btn'>
-              <span className='add-to-cart-msg'> Add to Bag </span>
-              <div className="price">{props.item.price}</div>
-            </button>
-          </div>
+          <button className='btn'>
+            <span className='add-to-cart-msg'> Add to Bag </span>
+            <div className="price">{props.item.price}</div>
+          </button>
         </div>
       </div>
-    </section>
   );
 }
